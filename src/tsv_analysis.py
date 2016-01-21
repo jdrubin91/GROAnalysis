d4aaed7db9e17a62077f69bbdc95be02a224f0cc
jdrubin91 <jd.rubin91@gmail.com>
2016-01-21 11:54:15 -0700

    updates

--- src/tsv_analysis.py
+++ src/tsv_analysis.py
@@ -46,7 +46,7 @@ def run(file1,file2):
                     X.append(d1[key][2]/d2[key][2])
                 
     F = plt.figure()
-    plt.hist(X,100)
+    plt.hist(X,bins=100)
     plt.savefig(savedir + 'tsv_fig.png')
     
     return "done"
