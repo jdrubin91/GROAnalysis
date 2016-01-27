dbc47479498d5ea96098fbcfa7ace758c54fd801
jdrubin91 <jd.rubin91@gmail.com>
2016-01-27 15:39:23 -0700

    updates

--- src/reflect_coverage.py
+++ src/reflect_coverage.py
@@ -5,10 +5,10 @@ def run(DMSO,CA,filedir):
     with open(DMSO) as F1:
         for line in F1:
             chrom, start, stop, coverage = line.strip().split()
-            if int(coverage) < 0:
-                coverage = -int(coverage)
+            if float(coverage) < 0:
+                coverage = -float(coverage)
             else:
-                coverage = int(coverage)
+                coverage = float(coverage)
             list1.append((chrom,start,stop,coverage))
     outfile1 = open(filedir + '/DMSO.bedgraph', 'w')
     for item in list1:
@@ -17,10 +17,10 @@ def run(DMSO,CA,filedir):
     with open(CA) as F2:
         for line in F2:
             chrom, start, stop, coverage = line.strip().split()
-            if int(coverage) < 0:
-                coverage = -int(coverage)
+            if float(coverage) < 0:
+                coverage = -float(coverage)
             else:
-                coverage = int(coverage)
+                coverage = float(coverage)
             list1.append((chrom,start,stop,coverage))
     outfile2 = open(filedir + '/CA.bedgraph', 'w')
     for item in list1:
