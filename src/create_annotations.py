52aff0325b51e5b45362df1a2683428063cde3f8
jdrubin91 <jd.rubin91@gmail.com>
2016-01-29 12:52:04 -0700

    updates

--- src/create_annotations.py
+++ src/create_annotations.py
@@ -18,10 +18,7 @@ def run(genes,filedir):
     outfile2 = open(filedir + '/END.bed', 'w')
     for item in list2:
         outfile2.write(item[0] + '\t' + str(item[1]) + '\t' + str(item[2]) + '\t' + item[3] + '\t' + item[4] + '\t' + item[5] + '\n')
-    #os.system("sort " + filedir + "/TSS.bed -k1,1 -k2,2n")
-    os.system("sortBed -i " + filedir + "/TSS.bed")
-    print "sortBed -i " + filedir + "/TSS.bed"
-    os.system("echo hello")
-    #> " + filedir + "/TSS.sorted.bed")
-    #> " + filedir + "/TSS.sorted.bed")
+    outfile.close()
+    outfile2.close()
+    os.system("sort " + filedir + "/TSS.bed -k1,1 -k2,2n > " + filedir + "/TSS.sorted.bed")
     os.system("sort " + filedir + "/END.bed -k1,1 -k2,2n > " + filedir + "/END.sorted.bed")
