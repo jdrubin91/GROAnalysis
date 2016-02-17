13ced4463b9b349bd8daa20f0a0576aa080cbd45
jdrubin91 <jd.rubin91@gmail.com>
2016-02-17 16:20:47 -0700

    updates

--- src/master_writer.py
+++ src/master_writer.py
@@ -4,6 +4,8 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from operator import itemgetter
+from scipy.stats import gaussian_kde
+import numpy as np
 
 def run(DMSOgenes,DMSOTSS,DMSOEND,CAgenes,CATSS,CAEND,filedir,figuredir):    
     d = dict()
@@ -57,6 +59,10 @@ def run(DMSOgenes,DMSOTSS,DMSOEND,CAgenes,CATSS,CAEND,filedir,figuredir):
     ENDlist = list()
     ENDgenes = list()
     DMSOENDgenes = list()
+    TRx = list()
+    TRy = list()
+    ERx = list()
+    ERy = list()
     cutoff2 = 0.01
     cutoff3 = 0.25
     i = 0
@@ -77,6 +83,10 @@ def run(DMSOgenes,DMSOTSS,DMSOEND,CAgenes,CATSS,CAEND,filedir,figuredir):
         CAEND = float(CAEND)
         if CAgenes-CATSS != 0 and DMSOgenes-DMSOTSS != 0 and CAgenes-CAEND != 0 and DMSOgenes-DMSOEND != 0 and DMSOgenes > coveragecutoff and CAgenes > coveragecutoff:
             i += 1
+            TRx.append(CATSS/(CAgenes-CATSS))
+            TRy.append(DMSOTSS/(DMSOgenes-DMSOTSS))
+            ERx.append(CAEND/(CAgenes-CAEND))
+            ERy.append(DMSOEND/(DMSOgenes-DMSOEND))
             TR = (CATSS/(CAgenes-CATSS))-(DMSOTSS/(DMSOgenes-DMSOTSS))
             if TR > cutoff1:
                 TRgenes.append((gene,TR))
@@ -94,6 +104,8 @@ def run(DMSOgenes,DMSOTSS,DMSOEND,CAgenes,CATSS,CAEND,filedir,figuredir):
     
     print "Genes: ",i
     
+    
+    
     F1 = plt.figure()
     TRlist.sort(reverse=True)
     #plt.hist(TRlist[int(len(TRlist)*.2):int(len(TRlist)*.8)],50)
@@ -109,6 +121,16 @@ def run(DMSOgenes,DMSOTSS,DMSOEND,CAgenes,CATSS,CAEND,filedir,figuredir):
     outfile2 = open(filedir + '/GeneList.txt','w')
     outfile2.write("High CA TR = " + str(len(TRgenes)) + "\nHigh DMSO TR = " + str(len(DMSOTRgenes)) + "\nHigh CA ER = " + str(len(ENDgenes)) + "\nHigh DMSO ER = " + str(len(DMSOENDgenes)) + "\n")
     outfile2.write("High CA TR\n")
+    F3 = plt.figure()
+    ax1 = F3.add_subplot(1,2,1)
+    xy = np.vstack([TRx,TRy])
+    z = gaussian_kde(xy)(xy)
+    ax1.scatter(TRx,TRy,c=z,edgecolor="",s=14)
+    ax2 = F3.add_subplot(1,2,2)
+    xy = np.vstack([TRx,TRy])
+    z = gaussian_kde(xy)(xy)
+    ax2.scatter(TRx,TRy,c=z,edgecolor="",s=14)
+    plt.savefig(figuredir + '/Scatter.png')
     for item in sorted(TRgenes, key=itemgetter(1),reverse=True):
         outfile2.write(item[0] + '\t' + str(item[1]) + '\n')
     outfile2.write("High DMSO TR\n")
