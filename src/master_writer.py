b91f3aa5ae2183f9ca3249a810336f42c6405120
jdrubin91 <jd.rubin91@gmail.com>
2016-02-17 17:38:13 -0700

    updates

--- src/master_writer.py
+++ src/master_writer.py
@@ -54,7 +54,7 @@ def run(DMSOgenes,DMSOTSS,DMSOEND,CAgenes,CATSS,CAEND,filedir,figuredir):
                 coverage = '0'
             d[gene].append(coverage)
             
-    coveragecutoff = 500
+    coveragecutoff = 200
     TRlist = list()
     TRgenes = list()
     DMSOTRgenes = list()
