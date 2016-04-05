7203c59d77c25a3e3ff33bf75c42de6c8b9554cd
jdrubin91 <jd.rubin91@gmail.com>
2016-04-05 11:29:26 -0600

    updates

--- src/tsv_analysis.py
+++ src/tsv_analysis.py
@@ -225,7 +225,7 @@ def run3(file1,file2):
                 if d2[key][2]-d1[key][2] < -.25:
                     #Z.append((key,d2[key][2]-d1[key][2]))
                     Z.append(key)
-                Y.append(d2[key][2]-d1[key][2])
+                Y.append(math.log(d2[key][2]/d1[key][2],2))
                 X.append(math.log((sum((d2[key][0],d2[key][1]))+sum((d1[key][0],d1[key][1])))/2.0,2))
     
     genedict = gene_dict(genes)
