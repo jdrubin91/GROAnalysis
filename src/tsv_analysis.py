2d811006f10a72af38e1ca265b09ede4c7626cab
jdrubin91 <jd.rubin91@gmail.com>
2016-04-05 11:15:12 -0600

    updates

--- src/tsv_analysis.py
+++ src/tsv_analysis.py
@@ -12,7 +12,7 @@ file2 = '/scratch/Shares/dowell/ENCODE/Rubin2016_genes_CA-1_K_models_MLE.tsv'
 savedir = '/scratch/Users/joru1876/GROAnalysis/figures/'
 genes = '/scratch/Users/joru1876/genome_files/refGene.bed'
 index = 6
-cut = 10
+cut = 0
 cut1 = 50000
 cut2 = -50000
 
