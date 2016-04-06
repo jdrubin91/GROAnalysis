dcce726b1b2a94cd618bd5ee029197f71c282a78
jdrubin91 <jd.rubin91@gmail.com>
2016-04-05 23:42:49 -0600

    updates

--- src/tsv_analysis.py
+++ src/tsv_analysis.py
@@ -8,10 +8,14 @@ from scipy.stats import norm
 import numpy as np
 import math
 
-file1 = '/scratch/Shares/dowell/ENCODE/Rubin2016_genes_DMSO-1_K_models_MLE.tsv'
-file2 = '/scratch/Shares/dowell/ENCODE/Rubin2016_genes_CA-1_K_models_MLE.tsv'
-savedir = '/scratch/Users/joru1876/GROAnalysis/figures/'
-genes = '/scratch/Users/joru1876/genome_files/refGene.bed'
+#file1 = '/scratch/Shares/dowell/ENCODE/Rubin2016_genes_DMSO-1_K_models_MLE.tsv'
+#file2 = '/scratch/Shares/dowell/ENCODE/Rubin2016_genes_CA-1_K_models_MLE.tsv'
+#savedir = '/scratch/Users/joru1876/GROAnalysis/figures/'
+#genes = '/scratch/Users/joru1876/genome_files/refGene.bed'
+file1 = 'C:/cygwin64/home/Jonathan/Rubin2016_genes_DMSO-1_K_models_MLE.tsv'
+file2 = 'C:/cygwin64/home/Jonathan/Rubin2016_genes_CA-1_K_models_MLE.tsv'
+savedir = 'C:/cygwin64/home/Jonathan/GROAnalysis/figures/'
+genes = 'C:/cygwin64/home/Jonathan/refGene.bed'
 index = 6
 cut = 16
 cut1 = 50000
@@ -28,8 +32,8 @@ def gene_dict(genes):
     genedict = dict()
     with open(genes) as F1:
         for line in F1:
-            key,gene = line.strip().split()[3].split(';')[0:2]
-            genedict[key] = gene
+            key,gene,position = line.strip().split()[3].split(';')[0:3]
+            genedict[key] = (gene,position)
             
     return genedict
 
@@ -195,8 +199,9 @@ def run3(file1,file2):
                 if '>' in line[0]:
                     line = line.strip().split('|')
                     gene = line[0][1:]
+                    position = line[1]
                     fwd, rev = line[2].split(',')
-                    d1[gene] = [float(fwd), float(rev)]
+                    d1[gene] = [float(fwd), float(rev), position]
                 elif '1' in line[1]:
                     p = line.strip().split()[index].split(',')[0]
                     d1[gene].append(float(p))
@@ -207,8 +212,9 @@ def run3(file1,file2):
                 if '>' in line[0]:
                     line = line.strip().split('|')
                     gene = line[0][1:]
+                    position = line[1]
                     fwd, rev = line[2].split(',')
-                    d2[gene] = [float(fwd),float(rev)]
+                    d2[gene] = [float(fwd),float(rev), position]
                 elif '1' in line[1]:
                     p = line.strip().split()[index].split(',')[0]
                     d2[gene].append(float(p))
@@ -220,14 +226,14 @@ def run3(file1,file2):
     genelist = list()
     for key in d1:
         if key in d2:
-            if (d1[key][0] > cut or d1[key][1] > cut) and (d2[key][0] > cut or d2[key][1] > cut) and d2[key][2] > 0 and d1[key][2] > 0:
+            if (d1[key][0] > cut or d1[key][1] > cut) and (d2[key][0] > cut or d2[key][1] > cut) and d2[key][3] > 0 and d1[key][3] > 0:
                 #if d2[key][2]-d1[key][2] > .25:
                     #Y.append((key,d2[key][2]-d1[key][2]))
                     #Y.append(key)
-                if d2[key][2]-d1[key][2] < -.25:
+                if d2[key][3]-d1[key][3] < -.25:
                     #Z.append((key,d2[key][2]-d1[key][2]))
                     Z.append(key)
-                Y.append(math.log(d2[key][2]/d1[key][2],2))
+                Y.append(math.log(d2[key][3]/d1[key][3],2))
                 X.append(math.log((sum((d2[key][0],d2[key][1]))+sum((d1[key][0],d1[key][1])))/2.0,2))
                 genelist.append(key)
                 
@@ -254,6 +260,7 @@ def run3(file1,file2):
     Y2 = list()
     genedict = gene_dict(genes)
     siglist = list()
+    bedlist = list()
     for i in range(len(X)):
         if int((X[i] - m)*10) < len(X1):
             k = int((X[i] - m)*10)
@@ -267,11 +274,20 @@ def run3(file1,file2):
             X2.append(X[i])
             Y2.append(Y[i])
             if genelist[i] in genedict:
-                siglist.append(genedict[genelist[i]])
+                siglist.append(genedict[genelist[i]][0])
+                bedlist.append(genedict[genelist[i]][1])
             else:
                 siglist.append(genelist[i])
+                bedlist.append(d1[genelist[i]][2])
             
-    
+    outfile = open('C:/cygwin64/home/Jonathan/GROAnalysis/files/tsv_analysis.bed','w')
+    window = (200)
+    for item in bedlist:
+        item = item.split(':')
+        chrom = item[0]
+        start = str(int(item[1].split('-')[0])-window)
+        stop = str(int(item[1].split('-')[0])+window)
+        outfile.write(chrom + '\t' + start + '\t' + stop + '\n')
     F = plt.figure() 
     ax = F.add_subplot(111)
     xy = np.vstack([X,Y])
@@ -279,11 +295,21 @@ def run3(file1,file2):
     plt.scatter(X,Y,c=z,edgecolor="",alpha=0.5,s=14) 
     plt.scatter(X2,Y2,c='r',edgecolor="",s=14)
     ax.set_xlim([4,20])
+    ax.set_title('Travel Ratio MA Plot')
+    ax.set_ylabel('log$_2$(TR$_{CA}$/TR$_{DMSO}$)')
+    ax.set_xlabel('log$_2$(Expression)')
     ax.get_xaxis().tick_bottom()
     ax.get_yaxis().tick_left()
     for l in range(len(X2)):
-        textx = np.random.uniform(-40,40)
-        texty = np.random.uniform(-40,40)
+        textx = 40
+        texty = 15
+        if X2[l] < 16 and Y2[l] < 0:
+            textx = 50
+            texty = -15
+        if siglist[l] == 'NR4A2':
+            texty = 15
+        if siglist[l] == 'PMF1-BGLAP' or siglist[l] == 'C2orf16':
+            texty = -25
         ax.annotate(siglist[l], xy=(X2[l],Y2[l]), xytext=(textx, texty), ha='right',
                 textcoords='offset points', 
                 arrowprops=dict(arrowstyle='->', shrinkA=0))
