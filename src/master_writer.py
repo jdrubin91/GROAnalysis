117989a7557b806994b7ea3b62e4af688dda988c
jdrubin91 <jd.rubin91@gmail.com>
2016-04-10 17:07:49 -0600

    updates

--- src/master_writer.py
+++ src/master_writer.py
@@ -353,8 +353,8 @@ def run(DMSOgenes,DMSOTSS,DMSOEND,CAgenes,CATSS,CAEND,filedir,figuredir):
     ax.set_title('Pearson Coefficient')
     ax.set_xlabel('DMSO genes')
     ax.set_ylabel('CA genes')
-    ax.set_xlim([0, .1])
-    ax.set_ylim([0, .1])
+    ax.set_xlim([0, 1000000])
+    ax.set_ylim([0, 1000000])
     ax.text(3,8, "Pearson coefficient: " + str(pearsons))
     plt.savefig(figuredir + '/Pearson.png')
     
