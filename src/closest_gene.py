daf5a9151b698549ecc0586392431b86137e3417
Jonathan Rubin <jonathanrubin@rl1-192-221-dhcp.int.colorado.edu>
2016-08-19 13:57:21 -0600

    updates

--- src/closest_gene.py
+++ src/closest_gene.py
@@ -41,9 +41,9 @@ def run(chipdir,refseq,filedir,DMSO,CA):
 	genes = filedir + '/SRF.gene.bed'
 
 	os.system("bedtools map -a " + genes + " -b " + DMSO + " -c 4 -o sum > " + filedir + "/DMSO.genes.bed")
-    os.system("bedtools map -a " + TSS + " -b " + DMSO + " -c 4 -o sum > " + filedir + "/DMSO.TSS.bed")
-    os.system("bedtools map -a " + genes + " -b " + CA + " -c 4 -o sum > " + filedir + "/CA.genes.bed")
-    os.system("bedtools map -a " + TSS + " -b " + CA + " -c 4 -o sum > " + filedir + "/CA.TSS.bed")
+	os.system("bedtools map -a " + TSS + " -b " + DMSO + " -c 4 -o sum > " + filedir + "/DMSO.TSS.bed")
+	os.system("bedtools map -a " + genes + " -b " + CA + " -c 4 -o sum > " + filedir + "/CA.genes.bed")
+	os.system("bedtools map -a " + TSS + " -b " + CA + " -c 4 -o sum > " + filedir + "/CA.TSS.bed")
 
     TRx = list()
     TRy = list()
