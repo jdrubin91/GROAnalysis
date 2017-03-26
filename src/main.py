560d03bc444aeb1eb0517ef2283f6faaf7d0f05d
Jonathan Rubin <jonathanrubin@cu-vpn-colorado-edu-198.11.29.61.int.colorado.edu>
2017-03-26 15:38:30 -0600

    updates

--- src/main.py
+++ src/main.py
@@ -42,10 +42,10 @@ figuredir = parent_dir(homedir) + '/figures'
 
 def run():
     #Specify DMSO treated bedgraph directory
-    DMSO = '/projects/dowellLab/Taatjes/170207_K00262_0069_AHHMHVBBXX/cat/trimmed/flipped/bowtie2/sortedbam/genomecoveragebed/fortdf/J52_trimmed.flip.fastq.bowtie2.sorted.BedGraph.mp.BedGraph'
+    DMSO = '/projects/dowellLab/Taatjes/170207_K00262_0069_AHHMHVBBXX/cat/trimmed/flipped/bowtie2/sortedbam/genomecoveragebed/fortdf/J12_trimmed.flip.fastq.bowtie2.sorted.BedGraph.mp.BedGraph'
 
     #Specify CA treated bedgraph directory
-    CA = '/projects/dowellLab/Taatjes/170207_K00262_0069_AHHMHVBBXX/cat/trimmed/flipped/bowtie2/sortedbam/genomecoveragebed/fortdf/J62_trimmed.flip.fastq.bowtie2.sorted.BedGraph.mp.BedGraph'
+    CA = '/projects/dowellLab/Taatjes/170207_K00262_0069_AHHMHVBBXX/cat/trimmed/flipped/bowtie2/sortedbam/genomecoveragebed/fortdf/J22_trimmed.flip.fastq.bowtie2.sorted.BedGraph.mp.BedGraph'
     
     #Specify gene annotations
     genes = '/scratch/Users/joru1876/GROSeqRaw/flipped/bowtie2/sortedbam/genomecoveragebed/fortdf/refGene.sorted.bed'
