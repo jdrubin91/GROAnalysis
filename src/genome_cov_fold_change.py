5e0761ee8e80dbd8e8267c0b1530733ddea92d7e
Jonathan Rubin <jonathanrubin@rl1-194-116-dhcp.int.colorado.edu>
2017-05-18 10:32:28 -0600

    updates

--- src/genome_cov_fold_change.py
+++ src/genome_cov_fold_change.py
@@ -39,9 +39,18 @@ def run(A2N,ACN,chromsizes,figuredir):
     for i in range(len(counts1)):
         int1 = counts1[i]
         int2 = counts2[i]
-        val1 = float(int1[-1])
-        val2 = float(int2[-1])
-        newbed.append(int1[:-1].append(math.log(val2/val1)))
+        if val1 != "0":
+            val1 = float(int1[-1])
+        else:
+            val1 = 0.001
+        if val2 != "0":
+            val2 = float(int2[-1])
+        else:
+            val2 = 0.001
+        try:
+            newbed.append(int1[:-1].append(math.log(val2/val1)))
+        except:
+            newbed.append(int1[:-1].append(0))
 
     print "done\ncreating and saving bedtool..."
     newbedtool = BedTool(newbed).saveas(parent_dir(A2N) + '/ACN_A2N_fold_change.BedGraph')
