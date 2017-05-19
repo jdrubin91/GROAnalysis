e442ec3ff6a5ad0b7a8fb6a08394b3cda10694c2
Jonathan Rubin <jonathanrubin@cu-vpn-colorado-edu-198.11.30.250.int.colorado.edu>
2017-05-19 16:05:38 -0600

    updates

--- src/AT_rich_gene_expression_changes.py
+++ src/AT_rich_gene_expression_changes.py
@@ -43,10 +43,10 @@ def convert_deseq_to_bed(deseqfile):
         for line in F:
             line.strip().split()
             if 'id' not in line[0] and 'NA' not in line[0]:
-                line = line[1].split(';')[-1]
-                chrom = line.split(':')[0]
-                start = line.split(':')[1].split('-')[0]
-                stop = line.split(':')[1].split('-')[1].split('_')[0]
+                item = line[1].split(';')[-1]
+                chrom = item.split(':')[0]
+                start = item.split(':')[1].split('-')[0]
+                stop = item.split(':')[1].split('-')[1].split('_')[0]
                 bed.append([chrom,start,stop])
 
     return bed
