e670855e007be9fbf25fb2558d5e2852a642629c
Jonathan Rubin <jonathanrubin@cu-vpn-colorado-edu-198.11.30.250.int.colorado.edu>
2017-05-22 19:40:24 -0600

    updates

--- src/differential_transcript_gc_content.py
+++ src/differential_transcript_gc_content.py
@@ -36,9 +36,9 @@ def get_tss(bedtool):
         chrom,start,stop = interval[:3]
         strand = interval[-1]
         if strand == '+':
-            newbedtool.append([chrom,str(int(start)-200),str(int(start)+200)])
+            newbedtool.append([chrom,str(int(start)-100),str(int(start)+100)])
         else:
-            newbedtool.append([chrom,str(int(stop)-200),str(int(stop)+200)])
+            newbedtool.append([chrom,str(int(stop)-100),str(int(stop)+100)])
 
     return BedTool(newbedtool)
 
