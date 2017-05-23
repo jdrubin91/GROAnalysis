45df1e8629cae6e5e294e8bf45ff500defb4c187
Jonathan Rubin <jonathanrubin@cu-vpn-colorado-edu-198.11.30.250.int.colorado.edu>
2017-05-22 20:32:33 -0600

    updates

---
+++ src/deseq_file_plotter.py
@@ -0,0 +1,58 @@
+__author__ = 'Jonathan Rubin'
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+rcParams.update({'figure.autolayout': True})
+import os
+import sys
+import pybedtools
+from pybedtools import BedTool
+import math
+import numpy as np
+
+def parent_dir(directory):
+    pathlist = directory.split('/')
+    newdir = '/'.join(pathlist[0:len(pathlist)-1])
+    
+    return newdir
+
+def run(deseqfile,cond1,cond2,figuredir):
+    with open(deseqfile) as F:
+        F.readline()
+        for line in F:
+            line = line.strip().split()
+            if 'NA' not in line[0]:
+                p = float(line[-1])
+                basemean = math.log(float(line[2]))
+                log2fc = float(line[-3])
+                if p < 0.01:
+                    sigx = math.log(float(line[2]))
+                    sigy = float(line[-3])
+
+    F = plt.figure()
+    ax = F.add_subplot(111)
+    ax.scatter(basemean,log2fc,alpha=0.5)
+    ax.scatter(sigx,sigy,c='r')
+    ax.set_title('Gene Transcription')
+    ax.set_ylabel('Log$_2$ Fold Change $' + cond2 + '/' + cond1 + '$')
+    ax.set_xlabel('Log$_10$ Mean Transcription')
+    plt.savefig(figuredir + deseqfile.split('/')[-1] + '.png', dpi=1200)
+
+
+if __name__ == "__main__":
+    #Home directory
+    homedir = os.path.dirname(os.path.realpath(__file__))
+
+    #File directory
+    filedir = parent_dir(homedir) + '/files/'
+    figuredir = parent_dir(homedir) + '/figures/'
+
+    cond1 = 'A2N'
+    cond2 = 'ACN'
+
+    deseqfile = '/projects/dowellLab/Taatjes/170413_K00262_0087_AHJLW5BBXX/cat/trimmed/flipped/bowtie2/sortedbam/genomecoveragebed/fortdf/DE-Seq/'+cond1+'_'+cond2+'.genes.bed.count.bed.'+cond1+cond2+'nascent.resSig_pvalue.txt'
+
+    run(deseqfile,cond1,cond2,figuredir)
+
