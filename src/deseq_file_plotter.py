43198d1521caaaf3508775e9b9097f207eb6f955
Jonathan Rubin <jonathanrubin@cu-vpn-colorado-edu-198.11.30.250.int.colorado.edu>
2017-05-23 19:20:13 -0600

    updates

--- src/deseq_file_plotter.py
+++ src/deseq_file_plotter.py
@@ -60,15 +60,15 @@ def run(deseqfile,cond1,cond2,figuredir,histone_names,cell_cycle_names,Sphase_na
                 if p < 0.01:
                     sigx.append(math.log(float(line[2])))
                     sigy.append(float(line[-3]))
-                if geneName in cell_cycle_names:
-                    ccx.append(math.log(float(line[2])))
-                    ccy.append(float(line[-3]))
+                # if geneName in cell_cycle_names:
+                #     ccx.append(math.log(float(line[2])))
+                #     ccy.append(float(line[-3]))
                 # if gene in histone_names:
                 #     hisx.append(math.log(float(line[2])))
                 #     hisy.append(float(line[-3]))
-                # if geneName in Sphase_names:
-                #     ccx.append(math.log(float(line[2])))
-                #     ccy.append(float(line[-3]))
+                if geneName in Sphase_names:
+                    ccx.append(math.log(float(line[2])))
+                    ccy.append(float(line[-3]))
                 # if geneName in DNArepair_names:
                 #     ccx.append(math.log(float(line[2])))
                 #     ccy.append(float(line[-3]))
@@ -102,8 +102,8 @@ def run(deseqfile,cond1,cond2,figuredir,histone_names,cell_cycle_names,Sphase_na
     ax.set_xlim([min(x),max(x)])
     plt.axhline(0, color='black',alpha=0.5)
     # plt.savefig(figuredir + deseqfile.split('/')[-1] + '_histones.png', dpi=1200)
-    plt.savefig(figuredir + deseqfile.split('/')[-1] + '_cell_cycle.png', dpi=1200)
-    # plt.savefig(figuredir + deseqfile.split('/')[-1] + '_S_phase.png', dpi=1200)
+    # plt.savefig(figuredir + deseqfile.split('/')[-1] + '_cell_cycle.png', dpi=1200)
+    plt.savefig(figuredir + deseqfile.split('/')[-1] + '_S_phase.png', dpi=1200)
     # plt.savefig(figuredir + deseqfile.split('/')[-1] + '.png', dpi=1200)
     # plt.savefig(figuredir + deseqfile.split('/')[-1] + '_DNA_repair.png', dpi=1200)
 
