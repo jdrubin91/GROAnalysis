b7d872f069e7dcd434fbfbe4469a7ae19cacab08
Jonathan Rubin <joru1876@colorado.edu>
2017-09-20 12:41:48 -0600

    add function in GSEA.py to sort intersect file

--- src/GSEA.py
+++ src/GSEA.py
@@ -2,6 +2,7 @@ __author__ = 'Jonathan Rubin'
 
 import os
 import sys
+import math
 
 def add_header(header,file1):
     lines = list()
@@ -36,8 +37,6 @@ def create_bidir_interval_file(filelist,filedir,condition1bam,condition2bam):
     os.system("sort -k1,1 -k2,2n " + filedir + "all_preliminary_bidir.merge.bed > " + filedir + "all_preliminary_bidir.merge.sort.bed")
     os.system("bedtools multicov -bams " + condition1bam + " " + condition2bam + " -bed " + filedir + "all_preliminary_bidir.merge.sort.bed >" + filedir + "all_preliminary_bidir.merge.sort.count.bed")
 
-    return filedir + "all_preliminary_bidir.merge.sort.count.bed"
-
 def create_intersect_file(interval_file,path_to_PSSMs,filedir):
     header = ['chrom','start','stop','cond1counts','cond2counts']
     i = 0
@@ -51,6 +50,40 @@ def create_intersect_file(interval_file,path_to_PSSMs,filedir):
         i += 1
 
     add_header('\t'.join(header)+'\n', filedir + "all_preliminary_bidir.merge.sort.count.intersect.bed")
+
+def sort_intersect_file(intersect_file,filedir):
+    fcs = list()
+    intervals = list()
+    with open(intersect_file) as F:
+        TFnames = F.readline().strip('\n').split('\t')[5:]
+        TFvals = [[]*len(TFnames)]
+        for line in F:
+            line = line.strip('\n').split('\t')
+            interval.append('\t'.join(line[:3]))
+            try:
+                log2fc = math.log(float(line[3])/float(line[4]),2)
+                fcs.append(log2fc)
+            else:
+                fcs.append(0)
+            for i in range(len(line[5:])):
+                val = line[5+i]
+                if val != 0:
+                    TFvals[i].append(1)
+                else:
+                    TFvals[i].append(0)
+
+    indices = [i[0] for i in sorted(enumerate(fcs), key=lambda x:x[1],reverse=True)]
+
+    outfile = open(filedir + filedir + "all_preliminary_bidir.merge.sort.count.intersect.sorted.bed",'w')
+    outfile.write('chrom\tstart\tstop\tlog2foldchange\t' + '\t'.join(TFnames) + '\n')
+    for i in indices:
+        outfile.write(intervals[i] + '\t' + str(fcs[i]))
+        for vals in TFvals:
+            outfile.write('\t' + vals[i])
+        outfile.write('\n')
+
+
+
 
 
 
@@ -70,15 +103,19 @@ if __name__ == "__main__":
                 folder2+'foot_print_testing-5_prelim_bidir_hits.bed',
                 folder2+'foot_print_testing-6_prelim_bidir_hits.bed']
     filedir = "/Users/joru1876/scratch_backup/GROAnalysis/files/"
+    figuredir = "/Users/joru1876/scratch_backup/GROAnalysis/figures/"
     bamfolder = '/projects/dowellLab/Taatjes/170207_K00262_0069_AHHMHVBBXX/cat/trimmed/flipped/bowtie2/sortedbam/'
     condition1bam = bamfolder + 'J52_trimmed.flip.fastq.bowtie2.sorted.bam'
     condition2bam = bamfolder + 'J62_trimmed.flip.fastq.bowtie2.sorted.bam'
     path_to_PSSMs = '/scratch/Shares/dowell/md_score_paper/PSSM_hits_genome_wide/pval-7/'
 
+    # create_bidir_interval_file(filelist,filedir,condition1bam,condition2bam)
+    # interval_file = filedir + "all_preliminary_bidir.merge.sort.count.bed"
+    # create_intersect_file(interval_file,path_to_PSSMs,filedir)
 
 
-    interval_file = create_bidir_interval_file(filelist,filedir,condition1bam,condition2bam)
-    create_intersect_file(interval_file,path_to_PSSMs,filedir)
 
+    intersect_file = filedir + "all_preliminary_bidir.merge.sort.count.intersect.bed"
+    sort_intersect_file(intersect_file,filedir)
 
 
